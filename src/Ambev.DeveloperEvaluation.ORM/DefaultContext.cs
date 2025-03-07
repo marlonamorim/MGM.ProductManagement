af981f2834c45e5700807517521c3f384fe382b3
Marlon Graciano <marlon.mgm@globo.com>
2025-03-07 18:31:32 -0300

    Add project files.

---
+++ src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
@@ -0,0 +1,39 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Ambev.DeveloperEvaluation.ORM;
+
+public class DefaultContext(DbContextOptions<DefaultContext> options) : DbContext(options)
+{
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("developer_evaluation");
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
+public class YourDbContextFactory : IDesignTimeDbContextFactory<DefaultContext>
+{
+    public DefaultContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<DefaultContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        builder.UseNpgsql(
+               connectionString,
+               b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.WebApi")
+        );
+
+        return new DefaultContext(builder.Options);
+    }
+}
